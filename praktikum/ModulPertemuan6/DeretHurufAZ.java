a458a4eeaa8fe8128a66c24849a722dcd8dfb377
furqon1728 <m.huwaidy23@gmail.com>
2023-06-02 15:36:29 +0700

    update latihan modul pert-7

--- praktikum/ModulPertemuan6/DeretHurufAZ.java
+++ praktikum/ModulPertemuan6/DeretHurufAZ.java
@@ -6,17 +6,28 @@ public class DeretHurufAZ {
 //        atau char yang dapat dicetak mewakili huruf, angka, tanda baca dan beberapa simbol lainnya
 
 //        dalam tabel ASCII character A = 65 dan Z = 90
-        char hurufKapitalAwal = 65, huruKapitalAkhir = 90;
-        System.out.println("Huruf Kapital");
-        for (char i = hurufKapitalAwal; i <= huruKapitalAkhir; i++){
+        char hurufKapitalAwal = 65, hurufKapitalAkhir = 90;
+        System.out.println("Huruf Kapital A-Z");
+        for (char i = hurufKapitalAwal; i <= hurufKapitalAkhir; i++){
             System.out.print(i + " ");
         }
 
 //        dalam tabel ASCII character a = 97 dan z = 122
         char hurufKecilAwal = 97, hurufKecilAkhir = 122;
-        System.out.println("\nHuruf Kecil");
+        System.out.println("\nHuruf Kecil a-z");
         for (char i = hurufKecilAwal; i <= hurufKecilAkhir; i++){
             System.out.print(i + " ");
         }
+
+        System.out.println("\nHuruf Kecil z-a");
+        for (char i = hurufKecilAkhir; i >= hurufKecilAwal; i--){
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nHuruf Kapital Z-A");
+        for (char i = hurufKapitalAkhir; i >= hurufKapitalAwal; i--){
+            System.out.print(i + " ");
+        }
+
     }
 }
--- praktikum/ModulPrakPertemuan7/TokoSerbaAda.java
+++
@@ -1,7 +0,0 @@
-package praktikum.ModulPrakPertemuan7;
-
-public class TokoSerbaAda {
-    public static void main(String[] args) {
-
-    }
-}
